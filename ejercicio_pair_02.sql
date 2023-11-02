2a4f321ed18b6c30d87fef305934709467b2c093
Miry2886 <miriamdiegoyjudith@gmail.com>
2023-11-02 11:32:56 +0100

    ejercicio terminado

--- ejercicio_pair_02.sql
+++ ejercicio_pair_02.sql
@@ -20,7 +20,13 @@
     pais char(45),
     codigo_postal char(45),   
     primary key (id_cliente));
- 
+  
+  create table zapatillas (
+	id_zapatilla int auto_increment not null,
+	modelo char not null,
+    color char not null,
+    primary key (id_zapatilla));
+    
  
  create table facturas (
 	id_factura int auto_increment not null,
@@ -29,26 +35,21 @@
     id_zapatilla int not null,
     id_empleado int not null,
     id_cliente int not null,
-    primary key (id_factura));
-    
+    primary key (id_factura),
+	
     constraint fk_empleados
-    foreign key (id_empleado)
-    references empleados(id_empleado)
+	foreign key (id_empleado)
+	references empleados(id_empleado),
     
     constraint fk_zapatillas
     foreign key (id_zapatilla)
-    references zapatillas(id_zapatilla)
+    references zapatillas(id_zapatilla),
     
     constraint fk_cliente
     foreign key (id_cliente)
-    references clientes(id_cliente)
+    references clientes(id_cliente));
         
-     
- create table zapatillas (
-	id_zapatilla int auto_increment not null,
-	modelo char not null,
-    color char not null)
-    
+		
     
  
  
